b61a7c98b51b0a0fd987556bcb169d64fcef7e43
Yadhunadh Chemmarassery <yadhu680@gmail.com>
2025-10-13 17:25:03 +0530

    Fix Netlify build dependency issue

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,7 +3,6 @@ import { Container } from "./styles";
 import { Hero } from "../Hero/Hero";
 import { About } from "../About/About";
 import { Contact } from "../Contact/Contact";
-import { Project } from "../Project/Project";
 import { Certificate } from "../Certificate/Certificate";
 import magento from "../../assets/magento.svg";
 import shopify from "../../assets/shopify.svg";
